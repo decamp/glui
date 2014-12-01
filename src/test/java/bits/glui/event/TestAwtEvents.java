3cccac668e97938e568cac0a8f6314946ed30578
decamp <decamp@users.noreply.github.com>
2014-11-30 19:51:20 -0500

    Updated libs. Added test code.

---
+++ src/test/java/bits/glui/event/TestAwtEvents.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2014. Massachusetts Institute of Technology
+ * Released under the BSD 2-Clause License
+ * http://opensource.org/licenses/BSD-2-Clause
+ */
+
+package bits.glui.event;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static java.lang.System.out;
+
+
+/**
+ * @author Philip DeCamp
+ */
+public class TestAwtEvents {
+
+
+    public static void main( String[] args ) throws Exception {
+        test1();
+    }
+
+
+    static void test1() {
+        final JFrame frame = new JFrame();
+        final JPanel pane   = new JPanel();
+        final JPanel panel0 = new JPanel();
+        final JPanel panel1 = new JPanel();
+
+        panel0.setBackground( Color.GRAY );
+        panel1.setBackground( Color.DARK_GRAY );
+        panel0.setFocusable( true );
+        panel1.setFocusable( true );
+
+        pane.add( panel0 );
+        pane.add( panel1 );
+        pane.setLayout( new LayoutManager() {
+            @Override
+            public void addLayoutComponent( String s, Component component ) {}
+
+            @Override
+            public void removeLayoutComponent( Component component ) {}
+
+            @Override
+            public Dimension preferredLayoutSize( Container container ) {
+                return null;
+            }
+
+            @Override
+            public Dimension minimumLayoutSize( Container container ) {
+                return null;
+            }
+
+            @Override
+            public void layoutContainer( Container container ) {
+                int w = container.getWidth();
+                int h = container.getHeight();
+                panel0.setBounds( 0, 0, w / 2, h );
+                panel1.setBounds( w / 2, 0, w - w / 2, h );
+                System.out.println( w + "\t" + h );
+            }
+        } );
+        frame.setContentPane( pane );
+
+        MouseHandler h = new MouseHandler();
+        panel0.addMouseListener( h );
+        panel0.addMouseMotionListener( h );
+        panel0.addMouseWheelListener( h );
+        panel0.addKeyListener( h );
+        panel1.addMouseListener( h );
+        panel1.addMouseMotionListener( h );
+        panel1.addMouseWheelListener( h );
+        panel1.addKeyListener( h );
+
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setSize( 512, 512 );
+        frame.setLocationRelativeTo( null );
+        frame.setVisible( true );
+    }
+
+
+    private static class MouseHandler implements MouseListener, MouseMotionListener, MouseWheelListener, KeyListener {
+
+
+        @Override
+        public void mouseClicked( MouseEvent event ) {
+            out.println( "    clicked:  " + format( event ) );
+        }
+
+        @Override
+        public void mousePressed( MouseEvent event ) {
+            out.println( "  pressed:  " + format( event ) );
+        }
+
+        @Override
+        public void mouseReleased( MouseEvent event ) {
+            out.println( "  released:  " + format( event ) );
+        }
+
+        @Override
+        public void mouseEntered( MouseEvent event ) {
+            out.println( "entered:  " + format( event ) );
+        }
+
+        @Override
+        public void mouseExited( MouseEvent event ) {
+            out.println( "exited:  " + format( event ) );
+        }
+
+        @Override
+        public void mouseDragged( MouseEvent event ) {
+//            out.println( "    dragged:  " + format( event ) );
+        }
+
+        @Override
+        public void mouseMoved( MouseEvent event ) {
+//            out.println( "    moved:  " + format( event ) );
+        }
+
+        @Override
+        public void mouseWheelMoved( MouseWheelEvent event ) {
+            out.println( "wheel:  " + format( event ) );
+        }
+
+        @Override
+        public void keyTyped( KeyEvent event ) {
+            out.println( "    typed: " + format( event ) );
+        }
+
+        @Override
+        public void keyPressed( KeyEvent event ) {
+            out.println( "  pressed: " + format( event ) );
+        }
+
+        @Override
+        public void keyReleased( KeyEvent event ) {
+            out.println( "  released: " + format( event ) );
+        }
+
+
+        String format( KeyEvent e ) {
+            String extra = String.format( "  code:%d  '%s'  %d", e.getKeyCode(), "" + e.getKeyChar(), e.getKeyLocation() );
+            return format( e, extra );
+        }
+
+
+        String format( MouseEvent e ) {
+            String extra = String.format( "(% 3d,% 3d) cc:%d  pop:%b", e.getX(), e.getY(), e.getClickCount(), e.isPopupTrigger() );
+            return format( e, extra );
+        }
+
+
+        String format( InputEvent e, String extra ) {
+            String mods   = String.format( "%16s", Integer.toBinaryString( e.getModifiers()   ) ).replace( ' ', '0' );
+            String exMods = String.format( "%16s", Integer.toBinaryString( e.getModifiersEx() ) ).replace( ' ', '0' );
+            return String.format( "%d %s\n    mods:   %s\n    modsEx: %s\n",
+                                  e.getID(),
+                                  extra,
+                                  mods,
+                                  exMods );
+        }
+
+
+    }
+
+}
+
---
+++ src/test/java/bits/glui/event/TestFocus.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2014. Massachusetts Institute of Technology
+ * Released under the BSD 2-Clause License
+ * http://opensource.org/licenses/BSD-2-Clause
+ */
+
+package bits.glui.event;
+
+import javax.swing.JFrame;
+
+import bits.draw3d.DrawEnv;
+import bits.draw3d.DrawStream;
+import bits.glui.*;
+
+/**
+* @author decamp
+*/
+public class TestFocus {
+
+
+    public static void main( String[] args ) {
+        test();
+    }
+
+
+    private static void test() {
+        GRootController cont = GRootController.create();
+
+        JFrame frame = new JFrame();
+        frame.setSize( 1024, 1024 + 30 );
+        frame.setLocationRelativeTo( null );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.add( cont.component() );
+
+        frame.setVisible( true );
+
+        int w = frame.getContentPane().getWidth();
+        int h = frame.getContentPane().getHeight();
+
+        cont.rootPane().addChild( newPanel( 0, 0, w, h, 3, 3, 3, new int[]{ 0 } ) );
+        cont.startAnimator( 40.0 );
+    }
+
+
+    private static Panel newPanel( int x, int y, int w, int h, int rows, int cols, int depth, int[] index ) {
+        Panel p = new Panel( index[0]++ );
+        p.bounds( x, y, w, h );
+        if( depth == 1 ) {
+            return p;
+        }
+
+        final int margin = 10;
+        for( int row = 0; row < rows; row++ ) {
+            for( int col = 0; col < cols; col++ ) {
+                int x0 = (w - margin) * col / cols + margin * 2;
+                int x1 = (w - margin) * (col + 1) / cols - margin;
+                int y0 = (h - margin) * row / rows + margin * 2;
+                int y1 = (h - margin) * (row + 1) / rows - margin;
+
+                p.addChild( newPanel( x0, y0, x1 - x0, y1 - y0, rows, cols, depth - 1, index ) );
+            }
+        }
+
+        return p;
+    }
+
+
+    private static class Panel extends GPanel implements GMouseListener, GMouseMotionListener, GMouseWheelListener, GFocusListener, GKeyListener {
+
+        private final int mIndex;
+        private boolean mHasFocus = false;
+        private boolean mHasMouse = false;
+        private boolean mMouseDown = false;
+        private boolean mMouse3Down = false;
+
+
+        public Panel( int index ) {
+            mIndex = index;
+            addMouseListener( this );
+            addMouseMotionListener( this );
+            addMouseWheelListener( this );
+            addFocusListener( this );
+            addKeyListener( this );
+        }
+
+
+        @Override
+        public void paintComponent( DrawEnv d ) {
+            final DrawStream s = d.drawStream();
+            float r = mMouse3Down ? 1 : 0;
+            float g = mHasMouse ? 1 : 0;
+            float b = mMouseDown ? 1 : 0;
+
+            s.config( true, false, false );
+            s.color( r, g, b );
+            s.beginQuads();
+            s.vert( 0, 0 );
+            s.vert( width(), 0 );
+            s.vert( width(), height() );
+            s.vert( 0, height() );
+            s.end();
+
+            if( !mHasFocus ) {
+                s.color( 0.5f, 0.5f, 0.5f );
+                d.mLineWidth.set( 1f );
+            } else {
+                s.color( 1f, 1f, 1f );
+                d.mLineWidth.set( 3f );
+            }
+
+            d.mCullFace.set( true );
+            s.beginLineLoop();
+            s.vert( 0, 0 );
+            s.vert( width(), 0 );
+            s.vert( width(), height() );
+            s.vert( 0, height() );
+            s.end();
+        }
+
+
+        public void mouseEntered( GMouseEvent e ) {
+            mHasMouse = true;
+        }
+
+
+        public void mouseExited( GMouseEvent e ) {
+            mHasMouse = false;
+        }
+
+
+        public void mousePressed( GMouseEvent e ) {
+            if( e.getButton() == 1 ) {
+                mMouseDown = true;
+
+            } else if( e.getButton() == 3 ) {
+                mMouse3Down = true;
+
+                if( (e.getModifiers() & GMouseEvent.META_DOWN_MASK) != 0 ) {
+                    if( (e.getModifiers() & GMouseEvent.SHIFT_DOWN_MASK) == 0 ) {
+                        System.out.println( "Start modal: " + mIndex );
+                        startModal();
+                    } else {
+                        System.out.println( "Stop modal: " + mIndex );
+                        stopModal();
+                    }
+                }
+            }
+
+            requestFocus();
+        }
+
+
+        public void mouseReleased( GMouseEvent e ) {
+            if( e.getButton() == 1 ) {
+                mMouseDown = false;
+            } else if( e.getButton() == 3 ) {
+                mMouse3Down = false;
+            }
+        }
+
+
+        public void mouseClicked( GMouseEvent e ) {
+            System.out.println( mIndex + " clicked : " + e.getClickCount() );
+        }
+
+        @Override
+        public void mouseMoved( GMouseEvent e ) {}
+
+        @Override
+        public void mouseDragged( GMouseEvent e ) {
+            System.out.println( this + "\t" + e.getX() + ", " + e.getY() );
+        }
+
+
+        public void focusGained( GFocusEvent e ) {
+            System.out.println( "Has focus: " + mIndex );
+            mHasFocus = true;
+        }
+
+
+        public void focusLost( GFocusEvent e ) {
+            mHasFocus = false;
+        }
+
+
+        public void keyPressed( GKeyEvent e ) {
+            System.out.println( mIndex + " pressed " + e.getKeyChar() + "\t" + e.getKeyCode() );
+        }
+
+
+        public void keyReleased( GKeyEvent e ) {
+            System.out.println( mIndex + " released " + e.getKeyChar() + "\t" + e.getKeyCode() );
+        }
+
+
+        public void keyTyped( GKeyEvent e ) {
+            System.out.println( mIndex + " typed " + e.getKeyChar() + "\t" + e.getKeyCode() );
+        }
+
+
+        public String toString() {
+            return "Panel " + mIndex;
+        }
+
+        @Override
+        public void mouseWheelMoved( GMouseWheelEvent e ) {
+            System.out.println( mIndex + " scrolled " + e.getScrollType() + "\t" + e.getScrollAmount() + "\t" + e.getWheelRotation() );
+        }
+    }
+
+}
